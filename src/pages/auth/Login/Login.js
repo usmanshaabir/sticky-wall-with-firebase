ea862bba7a8705d3f12ca18264d37b7bf6c16f22
usmanshaabir <mnaumanshabbir@gmail.com>
2023-09-01 15:46:44 +0500

    Completed

--- src/pages/auth/Login/Login.js
+++ src/pages/auth/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import "../../../scss/Login.scss"
 import { Button, Checkbox, Form, Input } from 'antd';
 import { auth } from '../../../config/firebase'
 import { useNavigate } from 'react-router-dom';
